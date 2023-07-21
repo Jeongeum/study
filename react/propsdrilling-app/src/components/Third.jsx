e8abe6ca62eb818932f702644d54935116097ee1
Jeongeum <wjddma1202@naver.com>
2023-07-21 16:18:53 +0900

    0721 props drilling 실습

---
+++ react/propsdrilling-app/src/components/Third.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Last } from './Last';
+
+export const Third = () => {
+  return (
+    <div>
+      <h2>Third component</h2>
+      <Last />
+    </div>
+  );
+};
