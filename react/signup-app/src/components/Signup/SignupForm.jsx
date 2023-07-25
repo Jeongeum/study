ddf0151ebac4e63e846fa2c4e61e7eb03dddc8c6
Jeongeum <wjddma1202@naver.com>
2023-07-25 15:00:33 +0900

    feat : 회원가입 기능 구현1_이메일, 비밀번호 입력 및 검증

--- react/signup-app/src/components/Signup/SignupForm.jsx
+++ react/signup-app/src/components/Signup/SignupForm.jsx
@@ -6,24 +6,37 @@ export const SignupForm = ({
   signupValue,
   onSubmitHandler,
   onChangeHandler,
+  emailMessage,
+  passwordMessage,
 }) => {
+  const { email, password } = signupValue;
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <TextCard
         id="email"
         type="email"
+        name="email"
         placeholder="이메일 주소를 입력해 주세요."
+        value={email}
+        onChange={onChangeHandler}
       >
         이메일
       </TextCard>
+      <span>{emailMessage}</span>
       <TextCard
         id="password"
         type="password"
+        name="password"
         placeholder="비밀번호를 설정해 주세요."
+        value={password}
+        onChange={onChangeHandler}
       >
         비밀번호
       </TextCard>
-      <Button type="submit">다음</Button>
+      <span>{passwordMessage}</span>
+      <Button type="submit" disabled={!email || !password}>
+        다음
+      </Button>
     </form>
   );
 };
--- react/signup-app/src/pages/Signup/SignupPage.jsx
+++ react/signup-app/src/pages/Signup/SignupPage.jsx
@@ -1,15 +1,86 @@
-import React from 'react';
-import Wrapper from '../../components/common/Wrapper';
+import React, { useEffect, useState } from 'react';
+import { Wrapper } from '../../components/common/Wrapper';
 
 import { SignupWrapper } from './styled';
 import { SignupForm } from '../../components/Signup/SignupForm';
+import { userEmailValid } from '../../api/auth';
+import { useNavigate } from 'react-router-dom';
 
 export const SignupPage = () => {
+  const [signupValue, setSignupValue] = useState({
+    email: '',
+    password: '',
+  });
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+  const [emailMessage, setEmailMessage] = useState('');
+  const [passwordMessage, setPasswordMessage] = useState('');
+  const navigate = useNavigate();
+
+  // 형식 검증 (입력할 때)
+  const isValid = (target, value) => {
+    if (target === 'email') {
+      const regEmail =
+        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+      if (!regEmail.test(value)) {
+        setIsValidEmail(false);
+        setEmailMessage('이메일 형식이 올바르지 않습니다.');
+      } else {
+        setIsValidEmail(true);
+        setEmailMessage('');
+      }
+    } else if (target === 'password') {
+      if (value.length < 6 && value.length !== '') {
+        setIsValidPassword(false);
+        setPasswordMessage('비밀번호는 6자 이상이어야 합니다.');
+      } else {
+        setIsValidPassword(true);
+        setPasswordMessage('');
+      }
+    }
+  };
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setSignupValue({ ...signupValue, [name]: value });
+    isValid(name, value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const data = {
+      user: { ...signupValue },
+    };
+    userEmailValid(JSON.stringify(data)).then((res) => {
+      if (res.message === '이미 가입된 이메일 주소입니다.') {
+        setEmailMessage(res.message);
+      } else if (res.message === '사용 가능한 이메일 입니다.') {
+        setEmailMessage(res.message);
+        navigate('/profilesetting', { state: signupValue });
+      } else {
+        console.error(res.message);
+      }
+    });
+  };
+
+  // useEffect(() => {
+  //   console.log(signupValue.email);
+  //   console.log('에러메시지: ', emailMessage, passwordMessage);
+  // }, [signupValue]);
+
   return (
     <Wrapper>
       <SignupWrapper>
         <h2>이메일로 회원가입</h2>
-        <SignupForm />
+        <SignupForm
+          signupValue={signupValue}
+          onSubmitHandler={onSubmitHandler}
+          onChangeHandler={onChangeHandler}
+          emailMessage={emailMessage}
+          passwordMessage={passwordMessage}
+        />
       </SignupWrapper>
     </Wrapper>
   );
