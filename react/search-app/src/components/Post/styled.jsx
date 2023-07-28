b20c262397d2fac623d30d47739fae13b54b20de
Jeongeum <wjddma1202@naver.com>
2023-07-28 17:25:50 +0900

    design : 영화목록 마크업 및 기본 이미지 추가

--- react/search-app/src/components/Post/styled.jsx
+++ react/search-app/src/components/Post/styled.jsx
@@ -7,26 +7,47 @@ export const PostListWrapper = styled.section`
   ul {
     display: grid;
     gap: 10px;
-    grid-template-columns: repeat(3, 300px);
+    grid-template-columns: repeat(3, 250px);
     justify-content: center;
     align-items: center;
     justify-items: center;
+    padding: 20px;
+    border-radius: 20px;
+    width: 800px;
+    margin: 0 auto;
+    background-color: #ffffff;
+    box-shadow: inset 0px 0px 10px rgb(155 152 152);
   }
 
   li {
+    position: relative;
     width: 100%;
+    height: 380px;
     background-color: #ffffff;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 10px;
-
+    text-align: center;
     border-radius: 20px;
-    box-shadow: 4px 4px 8px rgb(210 210 210);
-  }
 
-  img {
-    width: 250px;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 20px;
+    }
+
+    div {
+      width: 100%;
+      position: absolute;
+      bottom: 0px;
+      height: 80px;
+      border-radius: 10px 10px 20px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 10px;
+      font-weight: 600;
+
+      color: #ffffff;
+      background-color: rgba(255, 255, 255, 0.2);
+      backdrop-filter: blur(10px);
+    }
   }
 `;
