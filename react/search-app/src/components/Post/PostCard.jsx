6b0250059179ca4c2b01d8f1b6ab80e9c1daf5ec
Jeongeum <wjddma1202@naver.com>
2023-08-04 17:01:17 +0900

    feat : 이미지 404 에러 시 기본 이미지 불러오는 기능 구현

--- react/search-app/src/components/Post/PostCard.jsx
+++ react/search-app/src/components/Post/PostCard.jsx
@@ -3,10 +3,17 @@ import BasicImg from '../../assets/basic.png';
 
 export const PostCard = ({ item }) => {
   const { Poster, Title } = item;
+  const onErrorImg = (e) => {
+    e.target.src = BasicImg;
+  };
 
   return (
     <li>
-      <img src={Poster === 'N/A' ? BasicImg : Poster} alt={`${Title} poster`} />
+      <img
+        src={Poster === 'N/A' ? BasicImg : Poster}
+        alt={`${Title} poster`}
+        onError={onErrorImg}
+      />
       <div>{Title}</div>
     </li>
   );
