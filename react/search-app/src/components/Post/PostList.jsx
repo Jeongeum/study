ed875d79d90412506b7fc6ed13a138cce76b2935
Jeongeum <wjddma1202@naver.com>
2023-08-04 17:02:45 +0900

    feat : 무한스크롤 시 사용할 로딩스피너 설치 및 디바운스 커스텀 훅 구현

--- react/search-app/src/components/Post/PostList.jsx
+++ react/search-app/src/components/Post/PostList.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 import { PostCard } from './PostCard';
 import { PostListWrapper } from './styled';
+import { BeatLoader } from 'react-spinners';
 
 export const PostList = ({ movieData, refState }) => {
   return (
     <PostListWrapper>
-      {movieData.length > 0 && (
+      {movieData?.length > 0 && (
         <ul>
           {movieData?.map((item) => {
             return <PostCard item={item} key={item.imdbID} />;
           })}
-          <div ref={refState}></div>
+          <div ref={refState}>
+            <BeatLoader />
+          </div>
         </ul>
       )}
     </PostListWrapper>
