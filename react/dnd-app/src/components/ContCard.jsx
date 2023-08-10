bdd985d3573c2b9867f8a2192af8c0234d33104f
Jeongeum <wjddma1202@naver.com>
2023-08-10 17:27:57 +0900

    Update ContCard.jsx

--- react/dnd-app/src/components/ContCard.jsx
+++ react/dnd-app/src/components/ContCard.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import { ContCardWrapper } from './styled';
 
-export const ContCard = ({
-  children,
-  provided,
-  imgsrc,
-  dragStart,
-  dragEnter,
-  drop,
-  index,
-}) => {
+export const ContCard = ({ children, provided, imgsrc }) => {
   return (
     <ContCardWrapper
       imgsrc={imgsrc}
       draggable
-      // onDragStart={(e) => dragStart(e, index)}
-      // onDragEnter={(e) => dragEnter(e, index)}
-      // onDragEnd={drop}
-      // onDragOver={(e) => e.preventDefault()}
       ref={provided.innerRef}
       {...provided.draggableProps}
       {...provided.dragHandleProps}
