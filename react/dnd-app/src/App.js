6e47e63c0f88dc3c99add14d158a7c2454eb5e15
Jeongeum <wjddma1202@naver.com>
2023-08-11 17:21:44 +0900

    feat : dnd-kit을 이용해 grid 방식의 dnd 구현중

--- react/dnd-app/src/App.js
+++ react/dnd-app/src/App.js
@@ -1,15 +1,22 @@
 import { useState } from 'react';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-import addIcon from './assets/add.png';
 import {
-  AddBtn,
-  ContHeader,
-  ContListContainer,
-  InputFile,
-  Wrapper,
-} from './components/styled';
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  rectSortingStrategy,
+  SortableContext,
+  sortableKeyboardCoordinates,
+} from '@dnd-kit/sortable';
+
+import { ContHeader, Wrapper } from './components/styled';
 import { ImgBox } from './components/ImgBox';
-import { ContCard } from './components/ContCard';
+import { AddBox } from './components/AddBox';
 
 function App() {
   const [imgName, setImgName] = useState(new Array(4).fill(null));
@@ -26,13 +33,12 @@ function App() {
   };
 
   const onChangeImgHandler = (index, e) => {
+    const newList = [...imgList];
+    const newImgName = [...imgName];
+
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
-
-    const newList = [...imgList];
-
-    const newImgName = [...imgName];
 
     reader.onloadend = () => {
       newList[index] = { src: reader.result, name: file.name };
@@ -50,44 +56,66 @@ function App() {
 
     setImgList(newDeleteList);
   };
+  // -------------------------------------
 
-  // 드래그 시작될 때
-  // const dragStart = (e, position) => {
-  //   dragItem.current = position;
-  //   console.log('🖱 드래그 시작: ', e.target.alt);
-  // };
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
 
-  // 드래그 중인 대상이 위로 포개졌을 때
-  // const dragEnter = (e, position) => {
-  //   dragOverItem.current = position;
-  //   console.log(e.target.alt, '에 포개짐');
-  // };
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+    console.log('active: ', active);
+    console.log('over: ', over);
 
-  // 드래그를 끝냈을 때
-  const drop = (info) => {
-    const { destination, draggableId, source } = info;
-    const newImgList = [...imgList];
+    if (active.id !== over.id) {
+      console.log(active.id, over.id);
+      setImgList((imgList) => {
+        const oldIndex = imgList.indexOf(active.id); // imgList 배열에 active.id와 같은게 있으면 그 배열의 인덱스 반환, 없으면 -1 반환
+        const newIndex = imgList.indexOf(over.id);
+        console.log(oldIndex, newIndex);
+        return arrayMove(imgList, oldIndex, newIndex);
+      });
+      setImgName((imgName) => {
+        const oldIndex = imgName.indexOf(active.id) + 1;
+        const newIndex = imgName.indexOf(over.id) + 1;
 
-    if (!destination) return;
-    console.log('⭐ 지금 드래그 중인 대상: ', newImgList[source.index]);
-    console.log('⭐ 포개진 대상: ', newImgList[destination.index]);
-
-    const tmp = newImgList[source.index];
-    newImgList[source.index] = newImgList[destination.index];
-    newImgList[destination.index] = tmp;
-
-    // if (!destination) return;
-
-    // const [movedItem] = newImgList.splice(source.index, 1);
-    // newImgList.splice(destination.index, 0, movedItem);
-
-    console.log(
-      '현재 드래그 중인 대상과 포개진 대상을 맞교환한 리스트: ',
-      newImgList
-    );
-    setImgList(newImgList);
-    console.log('🖱 드래그 끝!');
+        return arrayMove(imgName, oldIndex, newIndex);
+      });
+    }
+    console.log(imgName);
+    console.log(imgList);
   };
+
+  // -------------------------------------
+  // 드래그를 끝냈을 때
+  // const drop = (info) => {
+  //   console.log('--------------drop-------------');
+  //   const { destination, draggableId, source } = info;
+  //   const newImgList = [...imgList];
+
+  //   if (!destination) return;
+  //   console.log('⭐ 지금 드래그 중인 대상: ', newImgList[source.index]);
+  //   console.log('⭐ 포개진 대상: ', newImgList[destination.index]);
+
+  //   // const tmp = newImgList[source.index];
+  //   // newImgList[source.index] = newImgList[destination.index];
+  //   // newImgList[destination.index] = tmp;
+
+  //   // if (!destination) return;
+
+  //   const [movedItem] = newImgList.splice(source.index, 1);
+  //   newImgList.splice(destination.index, 0, movedItem);
+
+  //   console.log(
+  //     '현재 드래그 중인 대상과 포개진 대상을 맞교환한 리스트: ',
+  //     newImgList
+  //   );
+  //   setImgList(newImgList);
+  //   console.log('🖱 드래그 끝!');
+  // };
 
   return (
     <Wrapper>
@@ -99,63 +127,118 @@ function App() {
           </h2>
           <p>설정 시, 내 채널 콘텐츠 상단에 4개까지 보여줍니다.</p>
         </ContHeader>
-        <DragDropContext onDragEnd={drop}>
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
+          <SortableContext items={imgList} strategy={rectSortingStrategy}>
+            {imgList.map((image, index) =>
+              image.src ? (
+                <ImgBox
+                  key={index}
+                  imgsrc={image.src}
+                  imgname={image.name}
+                  onClickDeleteHandler={onClickDeleteHandler}
+                  index={index}
+                />
+              ) : (
+                <AddBox
+                  key={index}
+                  onClickInputFileHandler={onClickInputFileHandler}
+                  onChangeImgHandler={onChangeImgHandler}
+                  index={index}
+                />
+              )
+            )}
+          </SortableContext>
+        </DndContext>
+
+        {/* ----------------- */}
+        {/* <DragDropContext onDragEnd={drop}>
           <Droppable droppableId="imageList">
             {(provided, snapshot) => (
               <ContListContainer
                 ref={provided.innerRef}
                 {...provided.droppableProps}
               >
-                {imgList.map((image, index) => (
-                  <Draggable
-                    key={index}
-                    draggableId={index.toString()}
-                    index={index}
-                  >
-                    {(provided) => (
-                      console.log(provided.draggableProps),
-                      (
-                        <ContCard
-                          key={index}
-                          imgsrc={image.src}
-                          index={index}
-                          provided={provided}
-                        >
-                          {image.src ? (
-                            <>
-                              <ImgBox
-                                imgsrc={image.src}
-                                imgname={image.name}
-                                onClickDeleteHandler={onClickDeleteHandler}
-                                index={index}
-                              />
-                            </>
-                          ) : (
-                            <>
-                              <AddBtn
-                                onClick={() => onClickInputFileHandler(index)}
-                              >
-                                <img src={addIcon} alt="등록하기 아이콘" />
-                                등록하기
-                              </AddBtn>
-                              <InputFile
-                                type="file"
-                                accept="image/*"
-                                id={`input_${index}`}
-                                onChange={(e) => onChangeImgHandler(index, e)}
-                              />
-                            </>
-                          )}
-                        </ContCard>
-                      )
-                    )}
-                  </Draggable>
-                ))}
+                {imgList.map(
+                  (image, index) => (
+                    console.log(
+                      'drag요소가 내 위에 있나요? :',
+                      snapshot.isDraggingOver
+                    ),
+                    console.log(
+                      '내 위로 지나가는 drag요소의 ID는? :',
+                      snapshot.draggingOverWith
+                    ),
+                    console.log(
+                      '여기서 출발하는 drag요소의 ID는? :',
+                      snapshot.draggingFromThisWith
+                    ),
+                    console.log(
+                      'placeholder 사용중인가요? :',
+                      snapshot.isUsingPlaceholder
+                    ),
+                    console.log('-------------------------------'),
+                    (
+                      <Draggable
+                        key={index}
+                        draggableId={index.toString()}
+                        index={index}
+                      >
+                        {(provided, snapshot) => (
+                          console.log(
+                            '제가 drag 되고 있나요? :',
+                            snapshot.isDragging
+                          ),
+                          console.log(
+                            'dropAnimation이 진행중인가요? :',
+                            snapshot.isDropAnimating
+                          ),
+                          console.log(
+                            'dropAnimation에 대한 정보 :',
+                            snapshot.dropAnimation
+                          ),
+                          console.log(
+                            '지금 어떤 droppable위를 지나가고 있나요?',
+                            snapshot.draggingOver
+                          ),
+                          (
+                            <ContCard
+                              key={index}
+                              imgsrc={image.src}
+                              index={index}
+                              provided={provided}
+                            >
+                              {image.src ? (
+                                <ImgBox
+                                  imgsrc={image.src}
+                                  imgname={image.name}
+                                  onClickDeleteHandler={onClickDeleteHandler}
+                                  index={index}
+                                />
+                              ) : (
+                                <AddBox
+                                  onClickInputFileHandler={
+                                    onClickInputFileHandler
+                                  }
+                                  onChangeImgHandler={onChangeImgHandler}
+                                  index={index}
+                                />
+                              )}
+                            </ContCard>
+                          )
+                        )}
+                      </Draggable>
+                    )
+                  )
+                )}
                 {provided.placeholder}
               </ContListContainer>
             )}
           </Droppable>
-        </DragDropContext>
+        </DragDropContext> */}
       </>
     </Wrapper>
   );
--- react/dnd-app/src/components/ImgBox.jsx
+++ react/dnd-app/src/components/ImgBox.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
-import { ContImg, DeleteIcon } from './styled';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { ContCardWrapper, ContImg, DeleteIcon } from './styled';
 import deleteIcon from '../assets/delete.png';
 // 이미지 등록 후 이미지가 미리보기로 보여지면 여기에서 드래그앤드롭 가능
 // 이미지 등록 전 + 표시가 있을 때에는 드래그앤드롭 불가능 (AddContCard)
 export const ImgBox = ({ imgsrc, imgname, onClickDeleteHandler, index }) => {
   const altName = imgname.trim().replace(/(.png|.jpg|.jpeg|.gif)$/, '');
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: index });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    cursor: 'pointer',
+    zIndex: isDragging ? '100' : 'auto',
+    opacity: isDragging ? 0.3 : 1,
+  };
   return (
-    <>
+    <ContCardWrapper
+      imgsrc={imgsrc}
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
       <ContImg src={imgsrc} alt={altName} />
       <DeleteIcon
         src={deleteIcon}
         alt="이미지 삭제"
         onClick={() => onClickDeleteHandler(index)}
       />
-    </>
+    </ContCardWrapper>
   );
 };
