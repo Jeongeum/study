521b74c09ed608b7358b1f64173be6f317f6ea72
Jeongeum <wjddma1202@naver.com>
2023-08-18 17:27:33 +0900

    feat : 지도 클릭 시 핀 찍기, 좌표를 주소로 바꾸는 기능 구현

--- react/navermap-practice/src/App.js
+++ react/navermap-practice/src/App.js
@@ -3,6 +3,8 @@ import myMarker from './assets/marker.png';
 
 function App() {
   const mapElement = useRef(null);
+  const [clickMap, setClickMap] = useState({ Lat: 0, Lng: 0 });
+  const [clickLocation, setClickLocation] = useState('');
   const [currentMyLocation, setCurrentMyLocation] = useState({
     Lat: 0,
     Lng: 0,
@@ -53,15 +55,51 @@ function App() {
     };
 
     // 지도 위에 표시될 마커
-    new naver.maps.Marker(markerOptions);
+    const marker = new naver.maps.Marker(markerOptions);
+
+    naver.maps.Event.addListener(map, 'click', async (e) => {
+      marker.setPosition(e.coord);
+      setClickMap((prev) => ({
+        // 클릭하는 곳의 좌표 알아내기
+        ...prev,
+        Lat: e.coord._lat,
+        Lng: e.coord._lng,
+      }));
+      naver.maps.Service.reverseGeocode(
+        {
+          coords: new naver.maps.LatLng(e.coord._lat, e.coord._lng),
+        },
+        (status, response) => {
+          if (status !== naver.maps.Service.Status.OK) {
+            return alert('Something wrong!');
+          }
+          const result = response.v2;
+          console.log(result);
+          setClickLocation(() => result.address.jibunAddress);
+        }
+      );
+    });
+    //   naver.maps.Service.reverseGeocode({
+    //     coords: new naver.maps.LatLng(clickMap.Lat, clickMap.Lng),
+    //   }) => {
+    //     if(status !== naver.maps.Service.Status.OK) {
+
+    //     }
+    //   }
+    // });
   }, [currentMyLocation]);
 
   useEffect(() => {
     getLocation();
   }, []);
-
-  console.log(currentMyLocation);
-
-  return <div ref={mapElement} style={{ minHeight: '400px' }} />;
+  console.log(clickLocation);
+  return (
+    <>
+      <div ref={mapElement} style={{ minHeight: '400px' }} />
+      <div>{clickMap.Lat}</div>
+      <div>{clickMap.Lng}</div>
+      <div>주소는 : {clickLocation}</div>
+    </>
+  );
 }
 export default App;
