135252a293676281bdf9c03f0f135e0b01a8a099
Jeongeum <wjddma1202@naver.com>
2023-09-20 17:21:43 +0900

    feat : 환경셋팅 관련 폴더 추가

---
+++ react/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>h1 입니다.</h1>
+      <h2>h1 입니다.</h2>
+      <h3>h1 입니다.</h3>
+      <h4>h1 입니다.</h4>
+      <h5>h1 입니다.</h5>
+      <h6>h1 입니다.</h6>
+    </div>
+  );
+}
+
+export default App;
